f1dee74e641ea6f7478fb7df1ec9e5dc1a013625
maj160 <13800690+maj160@users.noreply.github.com>
2023-01-02 00:33:14 +0000

    Use intrinsics for HBD SAD computation

--- src/asm/x86/dist/mod.rs
+++ src/asm/x86/dist/mod.rs
@@ -62,21 +62,84 @@ macro_rules! declare_asm_satd_hbd_fn {
   )
 }
 
-macro_rules! declare_asm_dist_xN_fn {
-  ($(($name: ident, $T: ident)),+) => (
-    $(
-      extern { fn $name (
-        src: *const $T, src_stride: isize, dst: *const $T, dst_stride: isize, n_rows : usize,
-      ) -> u32; }
-    )+
-  )
+#[cfg(target_arch = "x86_64")]
+use std::arch::x86_64::*;
+
+#[target_feature(enable = "avx2")]
+#[inline]
+unsafe fn mm_sum_i32(ymm : __m256i) -> i32 {
+  // We split the vector in half and then add 2 + 3 -> 0 + 1, and finally 0 + 1.
+  let m1 = _mm256_extracti128_si256(ymm, 1);
+  let m2 = _mm256_castsi256_si128(ymm);
+  let m2 = _mm_add_epi32(m2, m1);
+  let m1 = _mm_shuffle_epi32(m2, 0b11_10_11_10);
+  let m2 = _mm_add_epi32(m2, m1);
+  let m1 = _mm_shuffle_epi32(m2, 0b01_01_01_01);
+  let m2 = _mm_add_epi32(m2, m1);
+  _mm_cvtsi128_si32(m2)
 }
 
-declare_asm_dist_xN_fn![
-  (rav1e_sad_16xN_hbd_avx2, u16),
-  (rav1e_sad_32xN_hbd_avx2, u16),
-  (rav1e_sad_64xN_hbd_avx2, u16)
-];
+/// Performs a restricted 16-bit SAD over up to 256 elements.
+/// This is only valid if the inputs are 12-bit at most.
+/// Because we can accumulate 16 such values per AVX2 lane.
+#[target_feature(enable = "avx2")]
+#[inline]
+unsafe fn rav1e_sad_KxN_hbd_avx2_inner<const K : usize>(src: *const u16, src_stride: isize, dst: *const u16, dst_stride: isize, n_rows : usize) -> __m256i {
+
+  const LOADS_PER_REGISTER : isize = 32 / core::mem::size_of::<u16>() as isize; // 16
+  assert!(K * n_rows <= 256);
+  assert_eq!(K % LOADS_PER_REGISTER as usize, 0);
+  
+  let zero = _mm256_setzero_si256();
+  let src = src as *const i16;
+  let dst = dst as *const i16;
+  let mut sum16 = _mm256_setzero_si256();
+  for y in 0..n_rows as isize {
+    for x in 0..(K/16) as isize {
+      let a = _mm256_abs_epi16(_mm256_sub_epi16(
+        _mm256_loadu_si256(src.offset(x * LOADS_PER_REGISTER + y * src_stride / 2) as *const _),
+        _mm256_loadu_si256(dst.offset(x * LOADS_PER_REGISTER + y * dst_stride / 2) as *const _)
+      ));
+      sum16 = _mm256_add_epi16(sum16, a);
+    }
+  }
+
+  let b = _mm256_unpackhi_epi16(sum16, zero);
+  let c = _mm256_unpacklo_epi16(sum16, zero);
+
+  _mm256_add_epi32(b, c)
+}
+
+#[target_feature(enable = "avx2")]
+#[inline]
+unsafe fn rav1e_sad_KxN_hbd_avx2<const K : usize>(src: *const u16, src_stride: isize, dst: *const u16, dst_stride: isize, n_rows : usize) -> u32 {
+  if (K * n_rows <= 256) {
+    return mm_sum_i32(rav1e_sad_KxN_hbd_avx2_inner::<K>(src, src_stride, dst, dst_stride, row_step)) as u32;
+  }
+
+  // IF LPR = 16 and k =16, rows % 4
+  // If LPR = 16 and k = 64, rows % 1
+  const MAX_ELEMS : usize = 256;
+  let mut sum = _mm256_setzero_si256();
+  let col_step = K.min(MAX_ELEMS);
+  let row_step = n_rows.min(MAX_ELEMS / col_step);
+  assert!(row_step * col_step <= MAX_ELEMS);
+  assert_eq!(K % col_step, 0);
+  assert_eq!(n_rows % row_step, 0);
+  for w in 0..(n_rows / row_step) as isize {
+    for h in 0..(K / col_step) as isize {
+      let src = src.offset(w * src_stride / 2 * row_step as isize + h * col_step as isize);
+      let dst = dst.offset(w * dst_stride / 2 * row_step as isize + h * col_step as isize);
+      let res = if K > MAX_ELEMS {
+        rav1e_sad_KxN_hbd_avx2_inner::<MAX_ELEMS>(src, src_stride, dst, dst_stride, row_step)
+       } else {
+        rav1e_sad_KxN_hbd_avx2_inner::<K>(src, src_stride, dst, dst_stride, row_step)
+       };
+      sum = _mm256_add_epi32(sum, res);
+    }
+  }
+  mm_sum_i32(sum) as u32
+}
 
 declare_asm_dist_fn![
   // SSSE3
@@ -348,7 +411,7 @@ macro_rules! get_sad_hbd_avx2_WxN {
         unsafe extern fn [<rav1e_sad_ $W x $H _hbd_avx2>](
           src: *const u16, src_stride: isize, dst: *const u16, dst_stride: isize,
         ) -> u32 {
-          [<rav1e_sad_ $W x N _hbd_avx2>](
+          rav1e_sad_KxN_hbd_avx2::<[<$W>]>(
                 src,
                 src_stride,
                 dst,
@@ -374,39 +437,9 @@ get_sad_hbd_avx2_WxN!(
   (64,16),
   (64,32),
   (64,64),
-  (64,128)
-);
-
-macro_rules! get_sad_hbd_avx2 {
-  ($(($W:expr, $H:expr, $BS:expr)),*) => {
-    $(
-      paste::item! {
-        #[target_feature(enable = "avx2")]
-        unsafe extern fn [<rav1e_sad_ $W x $H _hbd_avx2>](
-          src: *const u16, src_stride: isize, dst: *const u16, dst_stride: isize,
-        ) -> u32 {
-          let mut sum = 0;
-          for w in (0..$W).step_by($BS) {
-            for h in (0..$H).step_by($BS) {
-              sum += [<rav1e_sad_ $BS x $BS _hbd_avx2>](
-                src.offset(w + h * src_stride / 2),
-                src_stride,
-                dst.offset(w + h * dst_stride / 2),
-                dst_stride
-              );
-            }
-          }
-          sum
-        }
-      }
-    )*
-  }
-}
-
-get_sad_hbd_avx2!(
-  // 64x64 base
-  (128, 128, 64),
-  (128, 64, 64)
+  (64,128),
+  (128, 64),
+  (128, 128)
 );
 
 static SAD_FNS_SSE2: [Option<SadFn>; DIST_FNS_LENGTH] = {
